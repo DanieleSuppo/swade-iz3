60834d1e1688cb53737e3c91a1b259c525eeff41
Daniele Suppo <dsuppo@zerogrey.com>
2022-05-15 18:18:48 +0200

    add ui to release

--- scripts/utils/functions.js
+++ scripts/utils/functions.js
@@ -7,6 +7,7 @@ export const createMaxStrain = async function (actor) {
                     dtype: "Number",
                     hasMaxValue: true,
                     label: game.i18n.localize("SWADE.Strain"),
+                    //TODO for v10: replace with data or system check discord?
                     max : actor.data.data.attributes.vigor.die.sides,
                     value: 0
                 }
@@ -20,6 +21,7 @@ export const updateMaxStrain = async function (actor, updateData) {
 
     let strain = null;
     let current = 0;
+    //TODO for v10: replace with actor.system?
     if(actor.data.data.additionalStats["strain"] !== undefined
         && actor.data.data.additionalStats["strain"].value !== null) {
         current = actor.data.data.additionalStats["strain"].value;
@@ -27,6 +29,7 @@ export const updateMaxStrain = async function (actor, updateData) {
     }
 
     //Add strain if not present
+    //TODO for v10: replace with with actor.system?
     if(actor.data.data.additionalStats["strain"] === undefined) {
         strain = actor.data.data.attributes.vigor.die.sides;
     }
